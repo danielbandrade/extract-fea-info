616317410e20e0a37b6960bdaf8823e621ad3cc2
Daniel Andrade <danielbarretoandrade@gmail.com>
2024-01-15 15:12:27 -0300

    making the search a funcition, first step

--- extract_fea_professors.js
+++ extract_fea_professors.js
@@ -4,24 +4,26 @@ const { read } = require('fs');
 
 const url = "https://www.fea.usp.br/economia/pessoas?field_nome_completo_value=&field_area_de_pesquisa_nid=All&field_linhas_de_pesquisa_nid=All&page=1";
 
+const professorsList = [];
+const professors = [];
 
 
 function readPage(url){
 
     
+    
+
     axios.get(url)
     .then(response => {
     const $ = cheerio.load(response.data);
-
-    const professors = [];
 
     // Adapte os seletores CSS de acordo com a estrutura HTML da página
     $('.view-pessoas-linha').each((index, element) => {
 
         const name = $(element).find('.views-field.views-field-field-nome-completo.view-pessoas-titulo').text().trim();
         const researchArea = $(element).find('.views-field.views-field-field-area-de-pesquisa.view-pessoas-miolo').text().trim();
-        const lattes = $(element).find('.views-field-field-curriculo-lattes.view-pessoas-miolo').text().trim(); 
-        const mail = $(element).find('.views-field-mail-1').text().trim();
+        const lattes = $(element).find('.views-field.views-field-field-curriculo-lattes.view-pessoas-miolo').text.trim(); 
+        const mail = $(element).find('.views-field-mail-1').text().trim('');
         // const nome = $(element).find('.views-field.views-field-field-nome-completo.view-pessoas-titulo').text().trim();
         // const nome = $(element).find('.views-field.views-field-field-nome-completo.view-pessoas-titulo').text().trim();
 
@@ -35,8 +37,9 @@ function readPage(url){
         };
 
         professors.push(infoProfessor);
-    });
 
+
+    });
 
     professors.forEach(professor => {
         console.log(professor);
@@ -47,11 +50,14 @@ function readPage(url){
     console.error("Error in request", error);
     });
 
-
 }
 
 
+
+
 readPage(url);
+
+
 
 
 
